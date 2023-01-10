f7b518afdbff58e6f5c6acd7321b3fc57b716e3a
unknown <aymmussa@gmail.com>
2023-01-10 23:28:46 +0000

    added readme file

---
+++ README.md
@@ -0,0 +1,23 @@
+# Password Generator
+
+## Overview
+
+This challenge was set for me to created a application that employee can use to generate a random password. the employee would also be able to set their criteria by selecting confirm or otherwise modifying the password generator to be resulted to their selection 
+
+## Built with
+HTML & CSS
+JavaScript
+
+## Installation 
+This application requires no installation. 
+
+This application runs in the browser.HTML and CSS and Javascript documents create a random password generator, using Javascript to make dynamic changes to an HMTL document. 
+
+## Instructions
+
+Clicking the generate button will promt the question on how many characters you would like your password to be. Which then follow up with further question to complete the series. After which the data is collected and a random password is generated using Javascript
+
+
+
+## Acknowledgment    
+edX Boot Camps
---
+++ script.js
@@ -0,0 +1,137 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+let passwordArray=[];
+let passwordlen=0
+
+// Function to prompt user for password options
+function getPasswordOptions() {
+
+  if (confirm ("Would you like to have UPPER CASE Characters? choose between 8-64!"));
+  if (!enter) {
+    alert("This needs a value");
+} else if (enter < 8 || enter > 128) {
+    // Validates user input
+    // Start user input prompts
+    enter = parseInt(prompt("You must choose between 8 and 64!"));{
+  passwordArray=passwordArray.concat(upperCasedCharacters)}
+
+    }
+}
+
+// Function for getting a random element from an array
+function getRandom(arr) {
+
+  let upperCasedCharacters
+
+
+}
+
+// Function to generate password with user input
+function generatePassword() {
+
+
+
+
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
